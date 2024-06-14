2e6ec3b0fb018583249bc6a22910109001f679dc
Kushal <dev@kushalkumarsaha.com>
2024-06-14 07:04:02 +0530

    Add files via upload

---
+++ src/common/types/types.ts
@@ -0,0 +1,8 @@
+
+
+export type ClassNameType                   = 'string'
+
+
+export type ThemeType                       = 'light' | 'dark'
+export type SocialLinkType                  = { label: string | JSX.Element, link:string }
+export type SVGType                         = { width?: number | string, height?: number | string, fill?:string, stroke?:string, className?:string }
