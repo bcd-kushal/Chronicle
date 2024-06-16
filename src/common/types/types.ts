64a3a861af74c2f9c7b3e9e4d752ebdd118a1806
Kushal <dev@kushalkumarsaha.com>
2024-06-16 23:57:30 +0530

    Add files via upload

--- src/common/types/types.ts
+++ src/common/types/types.ts
@@ -1,8 +1,15 @@
 
-
-export type ClassNameType                   = 'string'
+export type ClassNameType                   = string
 
 
 export type ThemeType                       = 'light' | 'dark'
 export type SocialLinkType                  = { label: string | JSX.Element, link:string }
+export type CategoryLinkType                = { label: string, link:string }
 export type SVGType                         = { width?: number | string, height?: number | string, fill?:string, stroke?:string, className?:string }
+
+
+export type SSGBlogStaticParamType          = Array<{ blogId: string }>
+
+
+
+export type NextResponseType                = { status: boolean, data: any | null, count?: number } 
