f2f404b0adfb54f0fa07431fc176a706821a52d6
Kushal <dev@kushalkumarsaha.com>
2024-06-16 23:59:35 +0530

    Add files via upload

--- src/app/(frontend)/[blogId]/layout.tsx
+++ src/app/(frontend)/[blogId]/layout.tsx
@@ -1,15 +1,21 @@
+import { GET_ALL_BLOGS_API_ROUTE } from "@/common/links/fetchRoutes"
+import { BLOGPAGE_SSG_REVLAIDATION } from "@/common/constants/revalidateIntervals"
 import { ChildrenType } from "@/common/types/reactTypes"
-import BlogHeader from "@/components/(frontend)/(blog)/BlogHeader/BlogHeader"
-import Comments from "@/components/(frontend)/(blog)/Comments/Comments"
-import Likes from "@/components/(frontend)/(blog)/Likes/Likes"
+import { Share2Icon, StarIcon, ThumbsDownIcon, ThumbsUpIcon } from "lucide-react"
+import LikesDislikes from "@/components/(frontend)/(blog)/LikesDislikes/LikesDislikes"
+import Favorite from "@/components/(frontend)/(blog)/Favorite/Favorite"
+
+export async function generateStaticParams() {
+    const url = GET_ALL_BLOGS_API_ROUTE()
+    const blogs = await fetch(url, { next: { revalidate: BLOGPAGE_SSG_REVLAIDATION } }).then(res => res.json())
+
+    return blogs
+}
 
 export default function BlogPostLayout({ children }: { children: ChildrenType }) {
     return (
         <>
-            <BlogHeader blogTitle="Some new blog" />
             {children}
-            <Likes />
-            <Comments />
         </>
     )
 }
