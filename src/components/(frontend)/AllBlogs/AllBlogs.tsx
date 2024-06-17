0f565cfc2f61c67d13a92110bdabcc08229800e1
Kushal <dev@kushalkumarsaha.com>
2024-06-17 23:39:51 +0530

    Add files via upload

--- src/components/(frontend)/AllBlogs/AllBlogs.tsx
+++ src/components/(frontend)/AllBlogs/AllBlogs.tsx
@@ -12,7 +12,7 @@ export default function AllBlogs({ category }: { category: string }) {
     
     return (
         <>
-        <section className="mt-2 mb-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-9 sm:gap-8 md:gap-10">
+        <section className="mt-2 mb-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 sm:gap-8 md:gap-10">
             {allBlogs.map((blog, index) => (
                 <BlogGhost blog={blog} page="category" key={index} />
             ))}
