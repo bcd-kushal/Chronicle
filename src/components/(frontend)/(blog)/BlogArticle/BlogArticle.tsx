1a748e7400f22092bdcda737226b240c3cc77c29
Kushal <dev@kushalkumarsaha.com>
2024-06-21 23:58:30 +0530

    Add files via upload

--- src/components/(frontend)/(blog)/BlogArticle/BlogArticle.tsx
+++ src/components/(frontend)/(blog)/BlogArticle/BlogArticle.tsx
@@ -1,3 +1,4 @@
+'use server'
 import styles from "@/decorators/blogData.module.css"
 import { EyeIcon, UserIcon, CalendarIcon, ThumbsUpIcon, MessageCircleIcon } from "lucide-react"
 import BlogGhost from "../BlogGhost/BlogGhost"
@@ -11,6 +12,8 @@ import Share from "../Share/Share"
 import BlogTopHeading from "./logic/BlogTopHeading"
 import { AUTHOR } from "@/common/constants/author"
 import { inThousands } from "@/common/utils/inThousands"
+import { __emptyFunction__ } from "@/common/utils/__emptyFunction__"
+import NewComment from "../../(atomic)/NewComment/NewComment"
 
 export default async function BlogArticle({ blogArticle, tag }: { blogArticle: string, tag: string }) {
     const relatedBlogs: BlogAtomicDataDocument[] = [
@@ -27,21 +30,22 @@ export default async function BlogArticle({ blogArticle, tag }: { blogArticle: s
         { key: <ThumbsUpIcon width={15} height={15} />, value: "32 likes" },
         { key: <EyeIcon width={15} height={15} />, value: "402 views" },
     ]
+
     return (
         <>
             <main className="grid px-3 min-[1200px]:px-0 grid-cols-1 md:grid-cols-[auto_380px]">
                 <BlogTopHeading date={new Date()} tag={tag} title="Understanding React Server Components" />
                 <span></span>
-                <MainBlogArea blogArticle={blogArticle} />
+                <MainBlogArea blogArticle={blogArticle} tag={String(tag)} />
                 <BlogMiscellaneousDetails date={new Date()} likes={2004} views={18390} relatedBlogs={relatedBlogs} />
             </main>
 
             {/* MOBILE UI BOTTOM STICKY ACTIONS TAB ============================== */}
             <div className="sticky bottom-0 px-3 bg-background md:hidden border-t-[1.5px] border-black/10 dark:border-white/15 flex items-center justify-between gap-5">
-                <LikesDislikes config={{ likes: 32, isSelected: true, selection: "like" }} type="default" />
+                <LikesDislikes likes={3401} selected="dislike" onLike={__emptyFunction__} onDislike={__emptyFunction__} type="default" />
                 <div className="flex items-center justify-end gap-2 py-2 text-black/70 dark:text-white/55">
-                    <Favorite className="" isSelected={false} />
-                    <Share className="" />
+                    <Favorite className="" isSelected={false} onSelect={__emptyFunction__} />
+                    <Share className="" blogName={tag} />
                 </div>
             </div>
         </>
@@ -83,17 +87,22 @@ function BlogMiscellaneousDetails({ date, likes, views, relatedBlogs }: { date: 
 }
 
 
-function MainBlogArea({ blogArticle }: { blogArticle: string }) {
+function MainBlogArea({ blogArticle, tag }: { blogArticle: string, tag: string }) {
     return (
         <div>
             <article dangerouslySetInnerHTML={{ __html: blogArticle }} className={styles.blogArticle} />
             <div className="flex items-center justify-between gap-3 pt-10">
                 <div className="flex items-center justify-start gap-1 md:gap-3">
-                    <LikesDislikes config={{ isSelected: true, likes: 2004, selection: "like" }} type="expanded" />
-                    <Favorite isSelected={false} className="hidden lg:block text-black/80 dark:text-white/80 transition-all duration-300 hover:text-black dark:hover:text-white" />
-                    <Share className="text-black/80 dark:text-white/80 transition-all duration-300 hover:text-black dark:hover:text-white" />
+                    <LikesDislikes likes={3401} selected="dislike" onLike={__emptyFunction__} onDislike={__emptyFunction__} type="expanded" />
+                    <Favorite isSelected={false} className="hidden lg:block text-black/80 dark:text-white/80 transition-all duration-300 hover:text-black dark:hover:text-white" onSelect={__emptyFunction__} />
+                    <Share className="text-black/80 dark:text-white/80 transition-all duration-300 hover:text-black dark:hover:text-white" blogName={tag} />
                 </div>
-                <span className="flex items-center justify-end gap-2 cursor-pointer text-black/80 dark:text-white/60 transition-all duration-300 hover:text-black dark:hover:text-white"><MessageCircleIcon height={18} width={18} /><span>Leave a Comment</span></span>
+                <NewComment blogTitle={tag} handleComment={__emptyFunction__} onCancel={__emptyFunction__}>
+                    <span className="flex items-center justify-end gap-2 cursor-pointer text-black/80 dark:text-white/60 transition-all duration-300 hover:text-black dark:hover:text-white">
+                        <MessageCircleIcon height={18} width={18} />
+                        <span>Leave a Comment</span>
+                    </span>
+                </NewComment>
             </div>
         </div>
     )
--- src/components/(frontend)/(blog)/BlogArticle/logic/BlogTopHeading.tsx
+++ src/components/(frontend)/(blog)/BlogArticle/logic/BlogTopHeading.tsx
@@ -6,6 +6,7 @@ import Tag from "@/components/(frontend)/Tag/Tag"
 import Favorite from "../../Favorite/Favorite"
 import Share from "../../Share/Share"
 import { BlogTopHeadingType } from "../utils/types"
+import { __emptyFunction__ } from "@/common/utils/__emptyFunction__"
 
 export default function BlogTopHeading({ title, tag, date }: BlogTopHeadingType) {
     return (
@@ -14,8 +15,8 @@ export default function BlogTopHeading({ title, tag, date }: BlogTopHeadingType)
             <Title config={{ type: "text", label: title, variant: "blog" }} />
             <div className="text-xs sm:text-sm flex flex-col sm:flex-row items-start sm:items-center justify-start gap-3 sm:gap-2">
                 <Tag label={tag} type="link" />
-                <Favorite className="hidden sm:block" isSelected={false} svgDimensions={20} />
-                <Share className="hidden sm:block" svgDimensions={20} />
+                <Favorite className="hidden sm:block" isSelected={false} onSelect={__emptyFunction__} svgDimensions={20} />
+                <Share className="hidden sm:block" blogName={tag} svgDimensions={20} />
             </div>
             <div className="text-zinc-700 dark:text-zinc-400 text-xs pt-2 flex items-center justify-start gap-2 mt-2 md:hidden"><CalendarIcon height={15} width={15} /><span>{moment(date).format(DATE_FORMAT.FULL)}</span></div>
         </section>
